5d2e62198b006529c4263cb9e1e315ee3e859359
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-17 22:47:57 -0500

    Create HelloWorld.java

---
+++ opp/HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld{
+    public static void main(String[]args){
+        System.out.println("Hello! World");
+        /* sout for system.out.println() */
+        System.out.println("Next Line");
+        
+        System.out.println("Java");
+        System.out.println("Is");
+        System.out.println("Language");
+
+        System.out.println("Escape sequence.\nIt\"is useful");
+
+        /* Task1
+         * Print the following using one System.out.println();
+         */
+
+         /*1.+ */
+         System.out.println("+");
+
+        /*2. ++ */
+        System.out.println("++");
+        /*3. +++ */
+        System.out.println("+++");
+        /*4. ++++ */
+        System.out.println("++++");
+        /*5. +++++*/
+        System.out.println("+++++");
+        
+
+    }
+}
