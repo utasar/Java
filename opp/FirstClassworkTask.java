bf457d973e5927f30ac99264385f2061fbe8fecf
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-17 22:46:56 -0500

    Create FirstClassworkTask.java

---
+++ opp/FirstClassworkTask.java
@@ -0,0 +1,80 @@
+/*Task 2. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as "*/ 
+
+public class FirstClassworkTask {
+    public static void main(String[]args)
+    {
+    System.out.println("A \"quoted\" String is");
+    System.out.println("'much' better if you learn");
+    System.out.println("the rules of \"escape sequences.\"");
+    System.out.println("Also, \"\" represents an empty");
+    System.out.println("String. Don't forget: use \\\"");
+    System.out.println("instead of \" !");
+    System.out.println("' is not the same as \"");
+
+ /*Task 3. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output.*/ 
+        System.out.print("This is my first program\n");
+        System.out.print("I am on module ST4003CEM\n");
+
+/*Task:4. WAP prints the following pattern:
+
+*********
+
+*********
+
+*********
+
+*********
+*/ 
+
+    System.out.println("*********");
+    System.out.println("*********");
+    System.out.println("*********");
+    System.out.println("*********");
+
+/*Task:6. WAP that prints the following output:
+
+A well-
+formed
+ Java
+  program
+  has a 
+  main
+   method 
+   with { 
+   and }
+   braces.*/ 
+
+        System.out.println("A well-");
+        System.out.println("formed");
+        System.out.println("Java");
+        System.out.println("program");
+        System.out.println("has a");
+        System.out.println("main");
+        System.out.println("method");
+        System.out.println("with {");
+        System.out.println("and }");
+        System.out.println("braces.");
+
+
+    }
+}
