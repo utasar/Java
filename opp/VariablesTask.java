fb58187d38e383d651df5d02411a612501bd5d5b
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-17 22:51:12 -0500

    Create VariablesTask.java

---
+++ opp/VariablesTask.java
@@ -0,0 +1,22 @@
+/*
+         * Task 
+         * Make an instance variable firstName 
+         * Make a new object and print the firstName 
+         * calculate and print the following 
+         * double avg =13/2
+         * float pct =411/100
+         */
+        public class VariablesTask{
+        String firstName="Pratikshya";
+
+        public static void main(String[] args) {
+            VariablesTask obj1 = new VariablesTask();
+        System.out.println("firstName: " + obj1.firstName);
+        double avg=13/2d;
+        System.out.println("Average: " + avg);
+        float pct = 411/100f;
+        System.out.println("Percentage: " + pct);
+
+
+            }
+}
