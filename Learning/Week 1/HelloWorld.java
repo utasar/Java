9081fa7e7082c7be4203f1273c846d1c51d3f5c2
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-22 22:20:58 -0500

    Create HelloWorld.java

---
+++ Learning/Week 1/HelloWorld.java
@@ -0,0 +1,34 @@
+
+/** @author Utsav
+ *
+ */
+public class HelloWorld
+{
+    /*
+    main method does x
+    y
+    and z
+    */
+    public static void main(String[] args)
+    {
+        // Prints to console
+        System.out.println("Hello World! Welcome to CS-1180!!"); // "say hi"
+
+        // Notice that the value of 'num' changes to 6 after we set num
+        // equal to 6 with the assignment operator
+        int num = 5;
+        System.out.print(num);
+        num = 6;
+        System.out.println(num);
+
+        // Demonstrates calling a method on an object
+        // An object is an instance of a class
+        String randomLetters = "AbCdEghIj";
+
+        // toLowerCase() returns the lowercase version of the String
+        // We need to assign this value back to our original String
+        // in order to save the lowercase version to the variable
+        randomLetters = randomLetters.toLowerCase();
+        System.out.println(randomLetters);
+    }
+}
