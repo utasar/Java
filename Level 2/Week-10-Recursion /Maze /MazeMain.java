51b86273a0cc382ac1978c0234c935ed1d34692b
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-24 23:42:46 -0500

    Create MazeMain.java

---
+++ Level 2/Week-10-Recursion /Maze /MazeMain.java
@@ -0,0 +1,7 @@
+public class MazeMain
+{
+   public static void main(String[] args) {
+      MazeGUI mazeWindow = new MazeGUI();
+      mazeWindow.go();
+   } // end method main  
+}//end of class Project3CS1181Fall2014
