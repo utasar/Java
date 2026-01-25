8fe52a24888052922dd04e0ac9e1347dfe80a868
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-24 22:58:37 -0500

    Create GUIIntro.java

---
+++ Level 2/Week-04-05-GUI /GUIIntro.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GUIIntro
+{
+    public static void main(String[] args)
+    {
+        JFrame window = new Window("Hello Window!");
+        System.out.println("All done!");
+    }
+}
