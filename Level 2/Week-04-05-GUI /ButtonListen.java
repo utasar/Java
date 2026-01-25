3ebbfc0d8deb599d56af16c43547b30563104529
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-24 22:56:27 -0500

    Create ButtonListen.java

---
+++ Level 2/Week-04-05-GUI /ButtonListen.java
@@ -0,0 +1,10 @@
+import java.awt.event.*;
+
+public class ButtonListen implements ActionListener
+{
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        System.out.println("I was pressed!");
+    }
+}
