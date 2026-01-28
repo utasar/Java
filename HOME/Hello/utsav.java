5d686a5321695013b29088f35276ec0149958ff0
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-27 21:17:48 -0500

    basic before

---
+++ HOME/Hello/utsav.java
@@ -0,0 +1,13 @@
+/* 
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+*/
+public class utsav {
+public static void main(String[] args) {
+    System.out.println("Hello World!!");
+}
+    
+}
