21630da644981e2335bf3e792267f470c0984a58
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-28 19:28:07 -0500

    Create App.java

---
+++ HOME/Labs/Lab 1/App.java
@@ -0,0 +1,12 @@
+public class App 
+{
+    public static void main(String[] args) throws Exception 
+    {
+     int i=-3;
+     int j=5;
+     double pi=3.14;
+     String s="Hello World";
+     String s1=s+pi;
+    System.out.println(s1);
+    }
+}
