7fa9dcdc90554b113bdae9d06eb4e8ffefaeb921
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-29 22:04:32 -0500

    Create App.java

---
+++ HOME/Class/Method /App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        printInfo("Muna", "Fairborn", "Talking");
+    }
+}
+public static void printInfo( String name, String state , String hobby)
+{
+
+}
