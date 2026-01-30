0c1cca0c234335872b4e67356e7df34f3a688be0
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-29 22:05:36 -0500

    Create PizzaMethods.java

---
+++ HOME/Class/Method /PizzaMethods.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/*       
+   * @author Utsav
+   * 
+ */
+public class PizzaMethods {
+    public static void main(String[] args) throws Exception 
+    {
+        
+        System.out.print("Enter Pizza Size");
+    }
+}
+public static double
