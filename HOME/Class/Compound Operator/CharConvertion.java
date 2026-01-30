e2cf919c23ea052f25ce50a8715909a6ce99fa24
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-29 22:11:25 -0500

    Create CharConvertion.java

---
+++ HOME/Class/Compound Operator/CharConvertion.java
@@ -0,0 +1,8 @@
+public class CharConvertion {
+    public static void main(String[] args) {
+        char firstAlpha='a';
+        char secondAlpha='b';
+        System.out.println(firstAlpha+secondAlpha);
+        System.out.println(" "+firstAlpha+secondAlpha);
+    }
+}
