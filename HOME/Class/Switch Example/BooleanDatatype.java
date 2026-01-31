96620ecf794bf0783c01dc53259889210f24f45b
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-30 22:29:23 -0500

    Create BooleanDatatype.java

---
+++ HOME/Class/Switch Example/BooleanDatatype.java
@@ -0,0 +1,30 @@
+public class BooleanDatatype {
+    public static void main(String[] args) {
+        boolean yesOrNo=true;
+        boolean isRaining=false;
+        String wordC="Hello".toLowerCase();
+
+        String wordA = "hello  ".trim();
+        String wordB ="hello";
+        if (wordA.equals(wordB))
+        {
+            System.out.println("two word A and B match");
+        }
+        else if(wordA.equals(wordC)){
+            System.out.println("Two words A and C Match");
+        }
+        else{
+            System.out.println("two words do not match");
+        }
+        // input string to character
+        char section = scnr.next.charAt(0);
+        if (section=='A'){
+
+        } 
+        else if (section=='B'){
+
+        }
+        else
+        System.out.println("Invalid Entry!!");
+    }
+}
