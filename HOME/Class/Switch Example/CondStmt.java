3f4479acc55cffeb6973ef19f66f9e35d678ef21
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-30 22:30:03 -0500

    Create  CondStmt.java

---
+++ HOME/Class/Switch Example/CondStmt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CondStmt {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int entry = scnr.nextInt();
+        if (entry==0){
+
+        }
+    }
+}
