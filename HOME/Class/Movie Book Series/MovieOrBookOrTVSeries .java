902e5b9c43fe1960beb13f34a799c85dc41594c1
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-30 22:27:25 -0500

    Create  MovieOrBookOrTVSeries .java

---
+++ HOME/Class/Movie Book Series/MovieOrBookOrTVSeries .java
@@ -0,0 +1,7 @@
+public class MovieOrBookOrTVSeries 
+{
+    public static void main(String[] args) 
+    { 
+        System.out.println("Westworld is my favourite TV series");//problem says to print one of the best option for book tV series or movie
+    }
+}
