6e843655242bda60c1153f654ad6b33ad3a9d89f
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-30 22:25:39 -0500

    Create BasicInput.java

---
+++ HOME/Class/Movie Book Series/BasicInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class BasicInput {
+    public static void main(String[] args) 
+    {
+        Scanner scnr = new Scanner(System.in);
+        String word= scnr.next();
+        System.out.println(word);
+        System.out.printf("%.3f\n", lengthKm);
+    }
+}
