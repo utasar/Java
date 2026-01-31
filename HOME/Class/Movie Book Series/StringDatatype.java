b186312b165bf08265c59b76e7ad738d6e20c9a2
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-30 22:27:51 -0500

    Create StringDatatype.java

---
+++ HOME/Class/Movie Book Series/StringDatatype.java
@@ -0,0 +1,12 @@
+public class StringDatatype {
+    public static void main(String[] args) {
+        String paragraphOnComputers,numbers;
+        paragraphOnComputers="A Computer is a machine that can be programmed to automatially carry out sequence of array";
+        numbers="12345678";
+        //output
+        System.out.println(paragraphOnComputers);
+        System.out.println(numbers);
+        
+        System.out.println(" \"Java\" is fun");
+    }
+}
