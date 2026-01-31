749c37c21703046bcd4e3bc9970ae241fbede046
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-30 22:28:19 -0500

    Create  Variables.java

---
+++ HOME/Class/Movie Book Series/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        //variable declaration
+        int numberOfStudents;
+        boolean trueOrFalse;
+        char firstLetter;
+        //variable assignment
+        numberOfStudents=2;
+        trueOrFalse=false;
+        firstLetter='a';
+        //output
+        System.out.print(numberOfStudents);
+        System.out.print("\n");//escape character
+        System.out.println(firstLetter);
+        System.out.print(trueOrFalse);
+
+        System.out.print("Hello\n");
+        System.out.print("World\n");
+
+        System.out.print("Hello"+"\t"+"World");
+    }
+}
