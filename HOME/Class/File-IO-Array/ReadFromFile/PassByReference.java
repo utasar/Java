1ba58049bee26a21d3bb053b264d05246e12ebd4
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-30 22:02:58 -0500

    Create PassByReference.java

---
+++ HOME/Class/File-IO-Array/ReadFromFile/PassByReference.java
@@ -0,0 +1,11 @@
+public class PassByReference {
+    public static void main(String[] args) {
+        int[] inputArguementArray ={1,2,3,4};
+        for(int i=0;i<inputArguementArray.length;i++)
+            {
+                System.out.println(inputArguementArray[i]);
+            }
+    }
+
+
+}
