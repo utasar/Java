92682e5c5d58e0cbf3340673de808ff9c30e7528
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-03-28 22:24:26 -0400

    Add FinishedShoppingEvent class for event handling

---
+++ Project/P3_GroceryStoreSim /FinishedShoppingEvent.java
@@ -0,0 +1,9 @@
+package P3_GroceryStoreSim;
+
+class FinishedShoppingEvent extends Event {
+
+	// Constructor
+	public FinishedShoppingEvent(Customer customer, double time) {
+		super(customer, time);
+	}
+}
